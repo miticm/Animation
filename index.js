dc73c03f9ad1430c9f508694385a7bda152a2667
Tianchi Xu <xtc459924276@gmail.com>
2018-07-07 15:33:06 +0800

    add play/pause

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-let animation = bodymovin.loadAnimation({
-  container:document.getElementById('bm'),
-  renderer:'svg',
-  loop:true,
-  autoplay:true,
-  path:'data.json'
+const options = {
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json'
+}
+
+let animation = bodymovin.loadAnimation(options)
+
+let bm = document.getElementById('bm')
+let isPause = false;
+bm.addEventListener('click', () => {
+  isPause ? animation.play() : animation.pause();
+  isPause = !isPause
 })
